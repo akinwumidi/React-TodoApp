85e2e7b308c03ff4d9fffd7350b9ee6d4a1ba42a
Israel Akinwumi <israelakin14@gmail.com>
2022-03-17 20:07:21 +0100

    Use SetState method to update checkbox on change

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -9,14 +9,36 @@ class TodoApp extends Component {
         this.state = {
             todos: todosData
         }
+        this.handleChange = this.handleChange.bind(this)
     }
-    handleChange = (id) => {
-        console.log('Changed', id)
+
+    handleChange(id) {
+        // console.log('Changed', id)
+
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    // todo.completed = !todo.completed
+                    let CurentValue;
+                    if (todo.completed === true) {
+                        CurentValue = false
+                    }
+                    todo.completed = CurentValue
+                }
+
+                return todo
+            })
+            return { todos: updatedTodos }
+
+        })
+
+
     }
+
     render() {
         const TodoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
         return (
-            <div className="TodoItemHolder">
+            <div className="TodoItemHolder" >
                 {TodoItems}
             </div>
         )
